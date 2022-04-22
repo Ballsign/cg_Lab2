01bfa01e70c1417480c49d4b8d8d23b2469ed5cb
Ballsign <zypok27@yandex.ru>
2022-04-22 12:43:54 +0500

    Tutor 6

--- cg_Lab2/cg_Lab2.cpp
+++ cg_Lab2/cg_Lab2.cpp
@@ -13,6 +13,28 @@ using namespace std;
 GLuint VBO;
 GLuint gWorldLocation;
 
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+}";
+
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0);                                           \n\
+}";
+
 
 void RenderSceneCB() {
     static float Scale = 0.0f;
@@ -20,43 +42,110 @@ void RenderSceneCB() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 1.0f, 0.0f);
 
-    /*glPushMatrix();*/
-    glLoadIdentity();//восстанавливаем нашу матрицу делая ее единичной
-    glTranslatef(0.5f, 0.5f, 0.0f);
-    glRotatef(Scale, 0.0f, 1.0f, 1.0f);
-    
+    glm::mat4  World;
 
+    World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = sinf(Scale);
+    World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
+    World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
+    World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
 
-    //glm::vec4 Position = glm::vec4(glm::vec3(0.0), 1.0);
-    //glm::mat4 Model = glm::mat4(1.0);
-    //Model = glm::vec4(1.0, 1.0, 0.0, 1.0);
-    //glm::vec4 Transformed = Model * Position;
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]);
 
-    //glEnableVertexAttribArray(0);
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
 
-    /*glBegin(GL_TRIANGLES);
-
-    glVertex2f(-0.5, 0.0);
-    glVertex2f(0.0, 0.5);
-    glVertex2f(0.5, 0.0);
-
-    glEnd();*/
-
-    glutWireTeapot(0.5);
-    glFlush();
-
-    /*glPopMatrix();*/
     Scale += 0.1f;
     
-    //glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(0);
 
     glutSwapBuffers();
 }
 
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+    glutIdleFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    glm::vec3 Vertices[3];
+    Vertices[0] = glm::vec3(0.0f, 0.33f, 0.0f);
+    Vertices[1] = glm::vec3(0.33f, 0.0f, 0.0f);
+    Vertices[2] = glm::vec3(-0.33f, 0.0f, 0.0f);
+
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText);
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    glCompileShader(ShaderObj);
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
+}
 
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
@@ -68,13 +157,13 @@ int main(int argc, char* argv[]) {
     glutCreateWindow("kirill rulz ok");
     glutDisplayFunc(RenderSceneCB);
 
-    //glm::vec3 Vertices[4];
-    //Vertices[0] = glm::vec3(-0.5f, 0.0f, 0.0f);
-    //Vertices[1] = glm::vec3(0.0f, 0.5f, 0.0f);
-    //Vertices[2] = glm::vec3(0.5f, 0.0f, 0.0f);
-    //Vertices[3] = glm::vec3(0, 0, 0);
+    glm::vec3 Vertices[4];
+    Vertices[0] = glm::vec3(-0.5f, 0.0f, 0.0f);
+    Vertices[1] = glm::vec3(0.0f, 0.5f, 0.0f);
+    Vertices[2] = glm::vec3(0.5f, 0.0f, 0.0f);
+    Vertices[3] = glm::vec3(0, 0, 0);
 
-
+    InitializeGlutCallbacks();
 
     GLenum res = glewInit();
     if (res != GLEW_OK)
@@ -85,19 +174,7 @@ int main(int argc, char* argv[]) {
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    //glGenBuffers(1, &VBO);
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-
-
-
-
-    //glm::mat4 myMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f));
-    //glm::vec4 myVector(10.0f, 10.0f, 10.0f, 1.0f);
-    //glm::vec4 transformedVector = myMatrix * myVector;
-
-
-    glutIdleFunc(RenderSceneCB);
+    CreateVertexBuffer();
+    CompileShaders();
     glutMainLoop();
 }
